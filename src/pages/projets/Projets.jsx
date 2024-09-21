b441240890cc498d499dd35576e5a0a05d4ab97e
EliseMairet <elise.mairet@hotmail.com>
2024-09-21 11:50:15 +0200

    commit 21/09

--- src/pages/projets/Projets.jsx
+++ src/pages/projets/Projets.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import projets from '../../data/projets.json'
-import Caroussel from '../../components/caroussel/Carrousel'
-import { useParams } from "react-router-dom"
-import './Projets.scss'
+  import React from 'react'
+  import projets from '../../data/projets.json'
+  import Caroussel from '../../components/caroussel/Carrousel'
+  import { Link, useParams } from "react-router-dom"
+  import './Projets.scss'
 
-const Projet = () => {
-  const { id } = useParams() // Récupérer l'ID depuis l'URL
+  const Projet = () => {
+    const { id } = useParams() // Récupérer l'ID depuis l'URL
 
-  // Trouver le projet correspondant à l'ID
-  const projet = projets.find((projet) => projet.id === id)
+    // Trouver le projet correspondant à l'ID
+    const projet = projets.find((projet) => projet.id === id)
 
-  // Si le projet n'est pas trouvé, afficher un message d'erreur ou rediriger
-  if (!projet) {
-    return <p>Projet non trouvé</p>
+    // Si le projet n'est pas trouvé, afficher un message d'erreur ou rediriger
+    if (!projet) {
+      return <p>Projet non trouvé</p>
+    }
+
+    return (
+      <div className="projet_container">
+        <div className="project_card">
+        <Link to="/" className="btn-retour">Retour à la page d'accueil</Link>
+          <h2>{projet.title}</h2>
+          {projet.pictures && projet.pictures.length > 1 && (
+            <Caroussel images={projet.pictures} />
+          )}
+          <p><strong>Catégorie:</strong> {projet.categorie}</p>
+          <p>{projet.description}</p>
+          <p><strong>Mission:</strong> {projet.mission}</p>
+          <p><strong>Tags:</strong> {projet.tags.join(', ')}</p>
+          <a href={projet.github} target="_blank" rel="noopener noreferrer">Voir sur GitHub</a>
+        </div>
+      </div>
+    )
   }
 
-  return (
-    <div className="projet_container">
-      <div className="project_card">
-        {projet.pictures && projet.pictures.length > 1 && (
-          <Caroussel images={projet.pictures} />
-        )}
-        <h2>{projet.title}</h2>
-        <p><strong>Catégorie:</strong> {projet.categorie}</p>
-        <p>{projet.description}</p>
-        <p><strong>Mission:</strong> {projet.mission}</p>
-        <p><strong>Tags:</strong> {projet.tags.join(', ')}</p>
-        <a href={projet.github} target="_blank" rel="noopener noreferrer">Voir sur GitHub</a>
-      </div>
-    </div>
-  )
-}
-
-export default Projet
+  export default Projet
