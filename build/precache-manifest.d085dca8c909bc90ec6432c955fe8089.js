ce737f8addf3fc450fc47689b2710a52082dca46
EliseMairet <elise.mairet@hotmail.com>
2024-09-23 13:48:23 +0200

    commit 23/09

---
+++ build/precache-manifest.d085dca8c909bc90ec6432c955fe8089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcec8745cd78d8a785d27edd13bfa16a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "74d179634773811ea5d1",
+    "url": "/portfolio/static/css/main.2ca85f40.chunk.css"
+  },
+  {
+    "revision": "ce0c86a50447e6a89d86",
+    "url": "/portfolio/static/js/2.7c7430f9.chunk.js"
+  },
+  {
+    "revision": "cbf92ed71fb4f2e94692ba67215d89b7",
+    "url": "/portfolio/static/js/2.7c7430f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d179634773811ea5d1",
+    "url": "/portfolio/static/js/main.272c33af.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Projet from './pages/projets/Projets'  
-import Accueil from './pages/accueil/Accueil'
-import './main.scss'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Projet from './pages/projets/Projets';
+import Accueil from './pages/accueil/Accueil';
+import './main.scss';
 
 const App = () => {
-  return(
-    <Router>
+  return (
+    <Router basename="/portfolio">
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/projects/:id" element={<Projet />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
